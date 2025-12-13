7cc2fbe69851d91e57a48d15fda58861a23db360
Hisoka <gintama2009@hotmail.ca>
2025-12-13 19:52:10 +0000

    feat: add skills in plain words

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -86,6 +86,14 @@ function App() {
 					title="AWS"
 				/>
 			</div>
+			<p>~ Or in plain words:</p>
+			<ul>
+				<li>
+					JavaScript, TypeScript, Node.js, HTML, CSS, PostgreSQL, React,
+					Express.js, Fastify, Git, Tailwind CSS, Prisma ORM, AWS Lambda, AWS
+					S3, AWS CloudFront, AWS API Gateway
+				</li>
+			</ul>
 		</div>
 	);
 }
