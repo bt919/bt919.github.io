50d9e71f5a67c6efe9ecf6f656532ff067f46795
Hisoka <gintama2009@hotmail.ca>
2025-12-15 20:54:18 +0000

    feat: add more themes and fix layout of themes buttons

--- src/routes/themes.tsx
+++ src/routes/themes.tsx
@@ -36,6 +36,39 @@ function RouteComponent() {
                     subColor={"#eceff4"}
                     mainColor={"#61afef"}
                 />
+
+                <ThemeButton
+                    theme="superuser"
+                    setCurrentTheme={(theme: string) => setCurrentTheme(theme)}
+                    bgColor={"#262a33"}
+                    textColor={"#e5f7ef"}
+                    subColor={"#526777"}
+                    mainColor={"#43ffaf"}
+                />
+                <ThemeButton
+                    theme="retro"
+                    setCurrentTheme={(theme: string) => setCurrentTheme(theme)}
+                    bgColor={"#dad3c1"}
+                    textColor={"#1d1b17"}
+                    subColor={"#918b7d"}
+                    mainColor={"#1d1b17"}
+                />
+                <ThemeButton
+                    theme="dracula"
+                    setCurrentTheme={(theme: string) => setCurrentTheme(theme)}
+                    bgColor={"#282a36"}
+                    textColor={"#f8f8f2"}
+                    subColor={"#6272a4"}
+                    mainColor={"#bd93f9"}
+                />
+                <ThemeButton
+                    theme="taro"
+                    setCurrentTheme={(theme: string) => setCurrentTheme(theme)}
+                    bgColor={"#b3baff"}
+                    textColor={"#130f1a"}
+                    subColor={"#6f6c91"}
+                    mainColor={"#130f1a"}
+                />
             </div>
         </div>
     );
